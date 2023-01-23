80c8e7fcb45241bc8a95801ada1d2ec24f2ea89e
Gor Yezekyan <gor.yezekyan@digitain.com>
2023-01-23 16:13:22 +0400

    SQS Publisher

---
+++ AwsFundametals/SqsPublisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+using System.Text.Json;
+
+using var sqsClient = new AmazonSQSClient();
+
+var customer = new CustomerCreated
+{
+	DateOfBirth = DateTime.Now,
+	Email = "gor.yezekyan@digitain.com",
+	FullName = "Gor Yezekyan",
+	GitHubUsername = "gyzk",
+	ID = Guid.NewGuid(),
+};
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("Customers");
+
+var sendMessageRequest = new SendMessageRequest
+{
+	QueueUrl = queueUrlResponse.QueueUrl,
+	MessageBody = JsonSerializer.Serialize(customer),
+	MessageAttributes = new Dictionary<string, MessageAttributeValue>
+	{
+		{ "string", new MessageAttributeValue{StringValue = nameof(CustomerCreated), DataType = "String" } }
+	}
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.ReadKey();
+
